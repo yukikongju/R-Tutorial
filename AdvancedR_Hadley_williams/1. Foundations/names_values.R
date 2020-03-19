6e0ea378107da029bd489653dcf01e4158f514cd
Emulie Chhor <emulie.chhor@hotmail.com>
2020-03-18 21:24:54 -0400

    made PCA example

--- AdvancedR_Hadley_williams/1. Foundations/names_values.R
+++ AdvancedR_Hadley_williams/1. Foundations/names_values.R
@@ -1,1 +1,22 @@
 # https://adv-r.hadley.nz/
+
+library(lobstr)
+
+# ------------------- Binding Basics: the name is the reference to a value. use syntaxic name ----------------------
+
+# each value has its own adress
+x <- c(1, 2, 3)
+y <- x
+obj_addr(x)
+obj_addr(y)
+
+# exercices
+
+obj_addr(mean)
+obj_addr(base::mean)
+obj_addr(evalq(mean))
+
+# when we import csv files, data might have non syntaxic name (ex: "_abc"), which means we have to use different syntax to retrieve the value. Name variables must not begin with . or _, nor be reserved words like if, else, or ...
+
+
+
