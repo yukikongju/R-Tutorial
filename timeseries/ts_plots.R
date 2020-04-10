c491ae34f6f85a69316bf2fd6826a160e4e48465
Emulie Chhor <emulie.chhor@hotmail.com>
2020-04-10 00:03:53 -0400

    added time series plots

--- timeseries/ts_plots.R
+++ timeseries/ts_plots.R
@@ -11,4 +11,60 @@ autoplot(AirPassengers) +
   xlab("Year") +
   ylab("Passengers")
 
+
+# -------------- Time Plot with facets -------------------
+
+autoplot(elecdaily[,c("Demand", "Temperature")], facets = TRUE)+
+  ylab("")+
+  ggtitle("Daily Demand in Victoria, Australia")
+
 # ------------------ Seasonal Plot -------------
+
+ggseasonplot(AirPassengers, year.labels = TRUE, year.labels.left = TRUE) +
+  ylab("Passengers") +
+  ggtitle("Air Passengers by seasons")
+
+# ------------------ Polar Seasonal Plot -------------
+
+ggseasonplot(AirPassengers, polar = TRUE)+
+  ylab("Passengers") +
+  ggtitle("Polar plot: Air Passengers by seasons")
+
+# --------- Seasonal Subseries Plots ------------------
+
+ggsubseriesplot(AirPassengers) + 
+  ylab("Passengers") +
+  ggtitle("Subseries Plot: Air Passengers by seasons")
+
+# ------------- Scatter plot matrices -----------------------
+
+autoplot(visnights[,1:4], facets=TRUE)+
+  ylab("Number of visitors each quarter (millions)")
+
+# ------------------- qplot ---------------------
+
+qplot(data = as.data.frame(elecdemand), Temperature, Demand )
+
+# -------------- Corrrelation plot --------------
+
+GGally:: ggpairs(as.data.frame(visnights[,1:5]))
+
+# ------------------ Lag plots -------------------
+
+d_beer <- window(ausbeer, start = 1985)
+gglagplot(d_beer)
+
+# ------------ autocorrelation plot ---------------
+
+
+
+
+
+
+
+
+
+
+
+
+
